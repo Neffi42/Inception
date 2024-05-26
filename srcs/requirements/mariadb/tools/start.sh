d36fc2c1f1988764affaaed5a7e118cfe242c454
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-05-26 12:33:00 +0200

    mariadb ok

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-service mysql start
-
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > init.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> init.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> init.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
-
-mysql < init.sql
-
-kill $(cat /var/run/mysqld/mysqld.pid)
+echo "CREATE DATABASE ${MYSQL_DATABASE};" > /etc/mysql/init.sql
+echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /etc/mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
 
 mysqld
