948ef52567c92f378fff15f1f8e8255e7365bb43
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-05-26 20:46:18 +0200

    first draft done

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+cd /var/www/html
+
 if [ -f ./wp-config.php ]
 then
 	echo "WordPress already downloaded"
 else
-	wget -c http://wordpress.org/latest.tar.gz
-	tar -xzf latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
-
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+    wp core download --allow-root
+    wp config create --allow-root \
+        --dbhost=$MYSQL_HOSTNAME \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD
+    wp core install --allow-root \
+        --url=https://www.$DOMAIN_NAME \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USR \
+        --admin_password=$WP_ADMIN_PWD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --skip-email
 fi
 
 php-fpm7.4 -F
